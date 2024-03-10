18841ea759b0e12f0bcc86da5a09e53789032675
서진 <dltjwls0529@gmail.com>
2024-03-10 17:31:45 +0900

    🍱 svg icons 추가

--- public/svgs/index.ts
+++ public/svgs/index.ts
@@ -1,3 +1,27 @@
 import LogoText from "./logo_text.svg";
+import DefaultProfile from "./default_profile.svg";
+import Menu from "./menu.svg";
+import Mail from "./mail.svg";
+import Github from "./github.svg";
+import Help from "./help.svg";
 
-export { LogoText };
+import GithubLogo from "./github_logo.svg";
+import GoogleLogo from "./google_logo.svg";
+
+import CircleCheck from "./circle_check.svg";
+import TriangleWarning from "./triangle_warning.svg";
+import OctagonWrong from "./octagon_wrong.svg";
+
+export {
+  LogoText,
+  DefaultProfile,
+  Menu,
+  Mail,
+  Github,
+  Help,
+  GithubLogo,
+  GoogleLogo,
+  CircleCheck,
+  TriangleWarning,
+  OctagonWrong,
+};
