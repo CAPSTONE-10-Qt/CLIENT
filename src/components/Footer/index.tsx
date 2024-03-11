328e6c04fc02228bf27d486487b05023d63c5c64
서진 <dltjwls0529@gmail.com>
2024-03-11 15:01:29 +0900

    💄 Footer 컴포넌트 작성

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,33 @@
+import { LogoText, Mail, Github, Help } from "../../../public/svgs";
+
+import styles from "./index.module.scss";
+import cs from "classnames/bind";
+const cx = cs.bind(styles);
+
+const Footer = () => {
+  return (
+    <div className={cx("container")}>
+      <LogoText />
+      <div className={cx("inner")}>
+        <a href='mailto:529539@ewha.ac.kr' className={cx("el")}>
+          <Mail />
+          이메일
+        </a>
+        <a
+          href='https://github.com/TEAM-ITERVIEW'
+          target='_blank'
+          className={cx("el")}
+        >
+          <Github />
+          GitHub
+        </a>
+        <a href='/' className={cx("el")}>
+          <Help />
+          개인정보처리방침
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
