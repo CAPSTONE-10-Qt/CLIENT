ca24ed54a0869050acfb6009d82688907e590dc5
서진 <dltjwls0529@gmail.com>
2024-03-27 02:49:40 +0900

    🐛 Controller 컴포넌트 이름 수정 및 적용

--- src/containers/interview/main/Controller/index.tsx
+++ src/containers/interview/main/Controller/index.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import React, { useState } from "react";
-import { useRecoilState } from "recoil";
-import { interviewDataState } from "@store/interview";
-import { useRecord } from "../useRecord";
+import React, { useEffect, useState } from "react";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { interviewDataState, interviewAllowState } from "@store/interview";
+import useRecord from "../useRecord";
 
 import {
   CamFalse,
@@ -22,15 +22,30 @@ import styles from "./index.module.scss";
 import cs from "classnames/bind";
 const cx = cs.bind(styles);
 
-const ControlSection = () => {
+const Controller = () => {
   const [interview, setInterview] = useRecoilState(interviewDataState);
-  const { questionNum, currentIndex, onlyVoice, isMicOn, isSpeakerOn } =
-    interview;
+  const {
+    questionNum,
+    questionList,
+    currentIndex,
+    onlyVoice,
+    isMicOn,
+    isSpeakerOn,
+  } = interview;
+  const { done } = useRecoilValue(interviewAllowState);
+
   const toggleSpeaker = () =>
     setInterview({ ...interview, isSpeakerOn: !interview.isSpeakerOn });
 
   const [isNotRec, setNotRec] = useState<boolean>(true);
-  const { onRecord, offRecord, seconds } = useRecord(isNotRec, setNotRec);
+  const { onRecord, offRecord, seconds } = useRecord(
+    isNotRec,
+    setNotRec,
+    questionList[currentIndex].id,
+  );
+  useEffect(() => {
+    setInterview({ ...interview, isMicOn: !isNotRec });
+  }, [isNotRec]);
 
   return (
     <div className={cx("container")}>
@@ -40,7 +55,7 @@ const ControlSection = () => {
             <p>면접자</p>
             <span>면접 중 변경 불가</span>
           </div>
-          <div className={cx("icon-flex")}>
+          <div className={cx("icon-flex", "non-clickable")}>
             {isMicOn ? <MicTrue /> : <MicFalse />}
             {onlyVoice ? <CamFalse /> : <CamTrue />}
           </div>
@@ -66,34 +81,38 @@ const ControlSection = () => {
         <div>
           <div className={cx("text-flex", "between")}>
             <p>면접 진행도</p>
-            <p>{`${Math.floor(currentIndex + (1 / questionNum) * 100)}% (${
-              currentIndex + 1
-            } of ${questionNum})`}</p>
+            <p>{`${
+              done ? 100 : Math.floor((currentIndex / questionNum) * 100)
+            }% (${done ? questionNum : currentIndex} of ${questionNum})`}</p>
           </div>
           <div className={cx("progress-bar")}>
             <div
               style={{
-                width: `${Math.floor(currentIndex + (1 / questionNum) * 100)}%`,
+                width: `${
+                  done ? 100 : Math.floor((currentIndex / questionNum) * 100)
+                }%`,
               }}
             />
           </div>
         </div>
       </div>
-      <div className={cx("time-text", isNotRec ? "off" : "on")}>
-        <span>답변 제한 시간</span>
-        <span id='record-timer-text'>{`0${Math.floor(seconds / 60)} : ${`0${
-          seconds % 60
-        }`.slice(-2)}`}</span>
-      </div>
-      <div
-        className={cx("control-button", isNotRec ? "off" : "on")}
-        onClick={isNotRec ? onRecord : offRecord}
-      >
-        {isNotRec ? <Play /> : <Stop />}
-        <p>{isNotRec ? "답변 시작" : "답변 완료"}</p>
+      <div className={cx("bottom-section")}>
+        <div className={cx("time-text", isNotRec ? "off" : "on")}>
+          <span>답변 제한 시간</span>
+          <span id='record-timer-text'>{`0${Math.floor(seconds / 60)} : ${`0${
+            seconds % 60
+          }`.slice(-2)}`}</span>
+        </div>
+        <div
+          className={cx("control-button", isNotRec ? "off" : "on")}
+          onClick={isNotRec ? onRecord : offRecord}
+        >
+          {isNotRec ? <Play /> : <Stop />}
+          <p>{isNotRec ? "답변 시작" : "답변 완료"}</p>
+        </div>
       </div>
     </div>
   );
 };
 
-export default ControlSection;
+export default Controller;
