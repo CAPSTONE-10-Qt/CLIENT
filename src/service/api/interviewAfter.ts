fb061c6ff7b2c0c7953f4c4e8bd95e3211698b39
서진 <dltjwls0529@gmail.com>
2024-05-07 17:55:24 +0900

    ✨ interviewAfter API 함수 형식 수정

--- src/service/api/interviewAfter.ts
+++ src/service/api/interviewAfter.ts
@@ -1,7 +1,7 @@
 import { backendClient } from ".";
 
 // 면접 상세 조회 - Server Fetch
-export async function getInterview(id: string) {
+export const getInterview = async (id: string) => {
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_BACKEND_SERVER_URL}/interview/result/${id}`,
     {
@@ -13,9 +13,9 @@ export async function getInterview(id: string) {
   }
   const { data } = await res.json();
   return data as InterviewDetailType;
-}
+};
 
-// 면접 리스트 조회
+// 면접 리스트 조회 - React Query
 export const getInterviewList = async (sort: number) => {
   const url = `/history/${sort}`;
   return backendClient.get(url);
@@ -27,20 +27,27 @@ export const patchQuestion = async (questionId: number) => {
   return backendClient.patch(url);
 };
 
-// 질문 상세 조회
+// 질문 상세 조회 - Server Fetch
 export const getQuestion = async (questionId: number) => {
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_BACKEND_SERVER_URL}/studynote/${questionId}`,
+    {
+      cache: "no-store",
+    },
   );
-  return await res.json();
+  if (!res.ok) {
+    throw new Error("질문 상세 조회 fetch 실패");
+  }
+  const { data } = await res.json();
+  return data as QuestionDetailType;
 };
 
-// 질문 리스트 조회
+// 질문 리스트 조회 - React Query
 export const getQuestionList = async (
   filter: number,
   subjectText: CSSubjectType | "ALL",
   onlyWrong: boolean,
 ) => {
-  const url = `/history/${filter}?subjectText=${subjectText}&onlyWrong=${onlyWrong}`;
+  const url = `/studynote/sortNum=${filter}&subjectText=${subjectText}&onlyWrong=${onlyWrong}`;
   return backendClient.get(url);
 };
