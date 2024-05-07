b0b135704aaee13aef898407a80e634e11e03057
서진 <dltjwls0529@gmail.com>
2024-05-07 17:49:16 +0900

    ✨ ReactQueryProviders 컴포넌트 작성

---
+++ src/service/hooks/index.tsx
@@ -0,0 +1,21 @@
+"use client";
+import { QueryClientProvider, QueryClient } from "react-query";
+import { useState } from "react";
+
+export default function ReactQueryProviders({
+  children,
+}: React.PropsWithChildren) {
+  const [client] = useState(
+    new QueryClient({
+      defaultOptions: {
+        queries: {
+          refetchOnWindowFocus: false,
+          refetchOnMount: false,
+          retry: 3,
+        },
+      },
+    }),
+  );
+
+  return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
+}
