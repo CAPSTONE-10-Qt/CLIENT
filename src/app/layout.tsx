2ce614e785e05fd762b5e3fe4e46a2b840580a8f
서진 <dltjwls0529@gmail.com>
2024-05-31 00:50:16 +0900

    🐛 favicon 재적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,21 +18,51 @@ const Pretendard = localFont({
 export const metadata: Metadata = {
   title: "iterview",
   description: "잇터뷰 : IT 취준생을 위한 CS 기술 구두 면접 대비 & 학습 플랫폼",
+  applicationName: "iterview",
+  metadataBase: new URL("https://iterview.vercel.app"),
+  authors: [
+    {
+      name: "이화여자대학교 졸업프로젝트 24-1 그로쓰 10팀 Qt",
+      url: "https://github.com/TEAM-ITERVIEW",
+    },
+  ],
   icons: {
     icon: [
+      "/favicon.ico",
       {
-        type: "image/svg+xml",
+        type: "image/png",
         media: "(prefers-color-scheme: light)",
-        url: "/favicon_light.svg",
+        url: "/favicon_light.png",
       },
       {
-        type: "image/svg+xml",
+        type: "image/png",
         media: "(prefers-color-scheme: dark)",
-        url: "/favicon_dark.svg",
+        url: "/favicon_dark.png",
+      },
+      {
+        rel: "apple-touch-icon",
+        type: "image/png",
+        url: "/apple-touch-icon.png",
       },
     ],
   },
+  openGraph: {
+    type: "website",
+    url: "https://iterview.vercel.app",
+    title: "iterview",
+    description:
+      "잇터뷰 : IT 취준생을 위한 CS 기술 구두 면접 대비 & 학습 플랫폼",
+    siteName: "iterview",
+    images: "/opengraph-image.png",
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "https://iterview.vercel.app",
+    creator: "",
+    images: "/twitter-image.png",
+  },
 };
+
 export const viewport: Viewport = {
   themeColor: "#333333",
 };
