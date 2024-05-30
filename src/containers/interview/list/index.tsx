a8558c706a7d41c38400b23fd517360f9cb5fd08
서진 <dltjwls0529@gmail.com>
2024-05-30 20:16:58 +0900

    🔥 mock data 코드 삭제

--- src/containers/interview/list/index.tsx
+++ src/containers/interview/list/index.tsx
@@ -2,6 +2,7 @@
 
 import InterviewBlock from "./InterviewBlock";
 import SortBar from "./SortBar";
+import Loader from "@components/Loader";
 
 import { useInterviewList } from "@service/hooks/interviewAfter";
 
@@ -10,14 +11,17 @@ import cs from "classnames/bind";
 const cx = cs.bind(styles);
 
 const InterviewListContainer = () => {
-  const { list } = useInterviewList();
+  const { list, isLoading } = useInterviewList();
   return (
     <div className={cx("container")}>
       <div className={cx("header")}>
         <h1>모의 면접 기록</h1>
         <SortBar />
       </div>
-      {list && !list.length ? (
+      <p>{list ? `총 ${list.length}개의 면접` : " "}</p>
+      {isLoading ? (
+        <Loader height='calc(100vh - 400px)' />
+      ) : list && list.length ? (
         <div className={cx("block-wrapper")}>
           {list.map((el: InterviewPreviewType) => (
             <InterviewBlock {...el} key={el.id} />
@@ -31,56 +35,3 @@ const InterviewListContainer = () => {
 };
 
 export default InterviewListContainer;
-
-const mock_data: InterviewPreviewType[] = [
-  {
-    subjectText: "OS",
-    startDateTime: "2024-3-10 18:23",
-    endDateTime: "2024-3-10 17:22",
-    totalTime: 680,
-    title: "2024.03.29. 모의 면접 2",
-    score: 1,
-    id: 2,
-    questionNum: 10,
-  },
-  {
-    subjectText: "OS",
-    startDateTime: "2024-3-10 18:23",
-    endDateTime: "2024-3-10 17:22",
-    totalTime: 3880,
-    title: "2024.03.29. 모의 면접 1",
-    score: 100,
-    id: 1,
-    questionNum: 20,
-  },
-  {
-    subjectText: "OS",
-    startDateTime: "2024-3-10 18:23",
-    endDateTime: "2024-3-10 17:22",
-    totalTime: 680,
-    title: "2024.03.29. 모의 면접 2",
-    score: 1,
-    id: 3,
-    questionNum: 10,
-  },
-  {
-    subjectText: "OS",
-    startDateTime: "2024-3-10 18:23",
-    endDateTime: "2024-3-10 17:22",
-    totalTime: 3880,
-    title: "2024.03.29. 모의 면접 1",
-    score: 100,
-    id: 4,
-    questionNum: 20,
-  },
-  {
-    subjectText: "OS",
-    startDateTime: "2024-3-10 18:23",
-    endDateTime: "2024-3-10 17:22",
-    totalTime: 3880,
-    title: "2024.03.29. 모의 면접 1",
-    score: 100,
-    id: 5,
-    questionNum: 20,
-  },
-];
--- src/containers/question/list/index.tsx
+++ src/containers/question/list/index.tsx
@@ -4,6 +4,7 @@ import Dropdown from "@components/Dropdown";
 import QuestionBlock from "./QuestionBlock";
 import FilterTab from "./FilterTab";
 import Toggle from "@components/Toggle";
+import Loader from "@components/Loader";
 
 import { useQuestionList } from "@service/hooks/interviewAfter";
 
@@ -21,13 +22,15 @@ const QuestionListContainer = () => {
       </div>
       <FilterTab />
       <div className={cx("top-info")}>
-        <p>{list && `총 ${list.length}개의 질문`}</p>
+        <p>{list ? `총 ${list.length}개의 질문` : " "}</p>
         <div>
           <span>정답 질문 제외하고 보기</span>
           <Toggle type='question' />
         </div>
       </div>
-      {list && !list.length ? (
+      {isLoading ? (
+        <Loader height='calc(100vh - 500px)' />
+      ) : list && list.length ? (
         <div className={cx("block-wrapper")}>
           {list.map((el: QuestionPreviewType) => (
             <QuestionBlock {...el} key={el.id} />
@@ -41,34 +44,3 @@ const QuestionListContainer = () => {
 };
 
 export default QuestionListContainer;
-
-const mock_data: QuestionPreviewType[] = [
-  {
-    id: 1,
-    questionText:
-      "Round Robin 스케쥴링에 대해 설명해주세요. Round Robin 스케쥴링에 대해 설명해주세요. Round Robin 스케쥴링에 대해 설명해주세요. Round Robin 스케쥴링에 대해 설명해주세요.",
-    subjectText: "OS",
-    score: 1,
-    title: "2023.12.25. 모의 면접 1 #2",
-    pin: 0,
-    again: 0,
-  },
-  {
-    id: 2,
-    questionText: "Round Robin 스케쥴링에 대해 설명해주세요.",
-    subjectText: "DB",
-    score: 0.5,
-    title: "2023.12.25. 모의 면접 1 #1",
-    pin: 1,
-    again: 1,
-  },
-  {
-    id: 3,
-    questionText: "Round Robin 스케쥴링에 대해 설명해주세요 설명해주세요.",
-    subjectText: "NW",
-    score: 0,
-    title: "2024.03.25. 모의 면접 1 #1",
-    pin: 1,
-    again: 0,
-  },
-];
